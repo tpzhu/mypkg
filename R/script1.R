4980d352f589cd42b0c2ffdb148a891787ccce29
Finn Roberts <robe2037@umn.edu>
2024-04-19 10:39:50 -0400

    Added function h

--- R/script1.R
+++ R/script1.R
@@ -6,6 +6,10 @@ g <- function(x){
   (x + 10 ) / 2
 }
 
+h <- function(x) {
+  f(stringr::str_length(x))
+}
+
 #use_r() to save definition of function automatically in /R directory.
 #load_all()
 #exists("f", where = globalenv(), inherits = FALSE)
