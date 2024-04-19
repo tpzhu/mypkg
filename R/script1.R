b3c17931c211da5576cdfc53bd1f7a4928d1cc3d
zhu00996 <zhu00996@umn.edu>
2024-04-19 11:11:33 -0400

    add l function

--- R/script1.R
+++ R/script1.R
@@ -6,6 +6,9 @@ g <- function(x){
   (x + 10 ) / 2
 }
 
+l <- function(x){
+  purrr::map(x, stringr::str_length)
+}
 #use_r() to save definition of function automatically in /R directory.
 #load_all()
 #exists("f", where = globalenv(), inherits = FALSE)
